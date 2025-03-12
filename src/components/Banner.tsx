a2b29eccbcd56522ab5ed41c4641964d1a60903a
NamiNaone99 <nongft09090@gmail.com>
2025-03-12 10:59:40 +0700

    modify word

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -4,7 +4,7 @@ const Banner = () => {
   return (
     <div className={styles.banner}>
       <div className={styles.overlay}>
-        <h1>Where every event finds its venue</h1>
+        <h1>where every event finds its venue</h1>
         <p>ให้บริการสถานที่จัดเลี้ยงในบรรยากาศสุดพิเศษ</p>
       </div>
     </div>
